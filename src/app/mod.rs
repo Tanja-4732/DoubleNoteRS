b90bf4c5eac627edb5297400c8b28ee4976d7715
Tanja <git@tanja.pw>
2023-06-24 00:44:40 +0200

    Begin work on `Welcome` screen

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,11 +1,15 @@
+mod routes;
+
 use leptos::*;
 use leptos_router::*;
+
+use routes::*;
 
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     view! { cx,
        <div>
-           <h1> {"Hello, world!"} </h1>
+        //    <h1> {"Hello, world!"} </h1>
            <MainView />
        </div>
     }
@@ -16,14 +20,15 @@ fn MainView(cx: Scope) -> impl IntoView {
     view! { cx,
       <Router>
         <nav>
-         <p> {"I'm a nav bar"} </p>
+        //  <p> {"I'm a nav bar"} </p>
         </nav>
         <main>
             //  <p> {"I'm the main content"} </p>
 
             // all our routes will appear inside <main>
         <Routes>
-            <Route path="/" view=|cx| view! { cx, <p> {"I'm the home page"} </p> } />
+            <Route path="/" view=|cx| view! { cx, <a href="welcome">{"welcome component"}</a> } />
+            <Route path="/welcome" view=|cx| view! { cx, <Welcome /> } />
             <Route path="/*any" view=|cx| view! { cx, <p> {"I'm a catch-all page"} </p> } />
       </Routes>
         </main>
