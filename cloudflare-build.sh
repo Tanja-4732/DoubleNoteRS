9be7eb7c9da397a632a427a11d90cefc20366506
Tanja <git@tanja.pw>
2023-06-23 19:09:21 +0200

    Add cloudflare-build.sh

---
+++ cloudflare-build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# cd into the direcoty of this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Install dependencies for build
+echo "Installing build dependencies for Cloudflare pages..."
+
+# Install rustup & Rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Add the .cargo/bin folder to PATH
+export PATH=$PATH:$HOME/.cargo/bin
+
+# Install the architecture wasm32-unknown-unknown
+rustup default nightly
+rustup target add wasm32-unknown-unknown
+
+# Install trunk
+cargo install --locked trunk
+
+echo "Finished installing build dependencies for Cloudflare pages."
+
+# Build the project
+trunk build --release
