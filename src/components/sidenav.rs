509589d6d7d02e7d194e09c9952b1e932d36d5f7
Tanja <git@tanja.pw>
2023-07-03 23:30:55 +0200

    Basic dark theme

--- src/components/sidenav.rs
+++ src/components/sidenav.rs
@@ -7,9 +7,9 @@ where
     IV: IntoView,
 {
     view! { cx,
-        <div id="main" class="flex flex-row w-full h-screen">
-            <div id="sidenav-menu" class="flex flex-col h-full min-w-fit">{nav_menu(cx)}</div>
-            <div id="sidenav-children" class="flex flex-col w-full">{children(cx)}</div>
+        <div id="main" class="flex flex-row w-full h-screen dark:bg-slate-800 dark:text-white">
+            <div id="sidenav-menu" class="flex flex-col h-full min-w-fit bg-gray-100 dark:bg-slate-900">{nav_menu(cx)}</div>
+            <div id="sidenav-children" class="flex flex-col w-full p-2">{children(cx)}</div>
         </div>
     }
 }
