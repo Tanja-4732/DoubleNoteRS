b4f0257995545c9ea5a5f62206a8ff063faa210a
Tanja <git@tanja.pw>
2023-07-07 21:51:32 +0200

    Sticky `Toolbar` (partial)

--- src/components/toolbar.rs
+++ src/components/toolbar.rs
@@ -13,7 +13,7 @@ pub fn Toolbar(cx: Scope, nav_toggle: WriteSignal<NavState>) -> impl IntoView {
         .expect("there to be a `title` signal provided");
 
     view! { cx,
-        <div class="w-full dark:bg-slate-700 dark:text-white p-2 text-xl">
+        <div class="w-full dark:bg-slate-700 dark:text-white p-2 text-xl sticky top-0 left-0">
             <button
                 class="dark:bg-green-500 px-2 max-w-fit rounded-full mr-2 overflow-hidden whitespace-nowrap text-clip"
                 on:click=move |_| nav_toggle.update(|state: &mut NavState| *state = state.toggle())
