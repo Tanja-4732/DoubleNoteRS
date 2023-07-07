9b3b59c8764ffbcb889382611eebad552a478335
Tanja <git@tanja.pw>
2023-07-07 03:25:44 +0200

    Improve `Settings`

--- src/app/routes/settings.rs
+++ src/app/routes/settings.rs
@@ -11,13 +11,11 @@ pub fn Settings(cx: Scope) -> impl IntoView {
     create_effect(cx, move |_| set_sidenav_type(overlay_type()));
 
     view! { cx,
-        <div>
-            <h1>{"Settings"}</h1>
-            <p>{"This is the settings page."}</p>
-            <ButtonGroup selection=overlay_type>
-                <button on:click=move |_| overlay_type.set(NavType::Overlay)>"Overlay"</button>
-                <button on:click=move |_| overlay_type.set(NavType::Push)>"Push"</button>
-            </ButtonGroup>
-        </div>
+        <h1>{"Settings"}</h1>
+        <p>{"This is the settings page."}</p>
+        <ButtonGroup selection=overlay_type>
+            <button on:click=move |_| overlay_type.set(NavType::Overlay)>"Overlay"</button>
+            <button on:click=move |_| overlay_type.set(NavType::Push)>"Push"</button>
+        </ButtonGroup>
     }
 }
