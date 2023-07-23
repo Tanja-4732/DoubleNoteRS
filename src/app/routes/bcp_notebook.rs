97113729d744f21eb449e01c2dbb3e0448399a61
Tanja <git@tanja.pw>
2023-07-23 20:44:51 +0200

    Initial edit dialog (got `<dialog>` to work with Leptos)

--- src/app/routes/bcp_notebook.rs
+++ src/app/routes/bcp_notebook.rs
@@ -7,6 +7,8 @@ use crate::core::{
     common::{set_title, NotebookType},
 };
 
+use crate::components::Card;
+
 #[component]
 pub fn BCPNotebook(cx: Scope, uuid: Uuid) -> impl IntoView {
     let my_notebook = bcp::BCPNotebook::new("My Notebook");
@@ -19,6 +21,13 @@ pub fn BCPNotebook(cx: Scope, uuid: Uuid) -> impl IntoView {
             <h1>{"Notebooks"}</h1>
             <p>{"This is the notebooks page."}</p>
             <h2>{"My Notebook"}</h2>
+            <Card buttons=move |_|{view! { cx,
+                <div>
+                    // <a href=href class="btn btn-primary">{"Open"}</a>
+                </div>
+            }}>
+                <p>{"This is my notebook."}</p>
+            </Card>
         </div>
     }
 }
--- src/app/routes/notebooks.rs
+++ src/app/routes/notebooks.rs
@@ -1,6 +1,9 @@
+use std::rc::Rc;
+
 use chrono::Utc;
 use gloo_storage::{Result, Storage};
-use leptos::{svg::view, *};
+use leptos::{html::Dialog, svg::view, *};
+use leptos_router::{use_params_map, use_query, use_query_map};
 
 use crate::{
     components::*,
@@ -12,7 +15,13 @@ use crate::{
 
 #[component]
 pub fn Notebooks(cx: Scope) -> impl IntoView {
-    set_title(cx, "Notebooks".into());
+    set_title(cx, "Notebooks");
+
+    let params = use_params_map(cx);
+    let query = use_query_map(cx);
+
+    // id: || -> Option<String>
+    // let id = move || params.with(|params| params.get("id").cloned());
 
     let notebooks: Result<Vec<BCPNotebook>> = gloo_storage::LocalStorage::get("dn2.bcp.notebooks");
     if notebooks.is_err() {
@@ -34,6 +43,15 @@ pub fn Notebooks(cx: Scope) -> impl IntoView {
         log::debug!("make_notebook")
     };
 
+    // let edit_dialog = Rc::new(Box::leak(Box::new(edit_notebook_dialog(cx))));
+    // let edit_dialog_2 = edit_dialog.clone();
+
+    // let open_edit = || edit_dialog.show_modal();
+
+    // edit_dialog.show_modal().unwrap();
+
+    let (open_edit_dialog, set_open_edit_dialog) = create_signal(cx, false);
+
     match notebooks {
         Ok(notebooks) => view! { cx,
             <div class="dn2-card-grid">
@@ -48,7 +66,7 @@ pub fn Notebooks(cx: Scope) -> impl IntoView {
                                     <a href=href.clone() class="bg-green-400 dark:bg-green-500 px-2 py-1 max-w-fit rounded">
                                         "Open"
                                     </a>
-                                    <a href=href.clone() class="border border-gray-500 dark:border-[#e5e7eb] mr-1 px-2 py-1 max-w-fit rounded">
+                                    <a on:click=move|_| { log::debug!("set edit dialog open"); set_open_edit_dialog(true); } class="border border-gray-500 dark:border-[#e5e7eb] mr-1 px-2 py-1 max-w-fit rounded">
                                         "Edit"
                                     </a>
                                 }
@@ -65,6 +83,8 @@ pub fn Notebooks(cx: Scope) -> impl IntoView {
             <button on:click=make_notebook class="dark:border mt-2 px-2 py-1 max-w-fit rounded" disabled>
                 "Import"
             </button>
+            <EditNotebookDialog opened=open_edit_dialog/>
+
         },
         Err(err) => view! { cx,
             <>
@@ -76,4 +96,23 @@ pub fn Notebooks(cx: Scope) -> impl IntoView {
             </>
         },
     }
+}
+
+#[component]
+fn edit_notebook_dialog(cx: Scope, opened: ReadSignal<bool>) -> impl IntoView {
+    let dialog = view! {cx,
+        <dialog>
+            "Hi there"
+        </dialog>
+    };
+
+    let dialog = store_value(cx, dialog);
+
+    create_effect(cx, move |_| {
+        if opened() {
+            dialog.with_value(|d| d.show_modal()).unwrap();
+        }
+    });
+
+    dialog
 }
